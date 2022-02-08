82b2d57939c5966dd9e322222026ff9f17aca102
MaxenceThacker <maxence.thacker@hotmail.fr>
2022-02-08 16:25:32 +0100

    Projet northwind: Controller, Templates

---
+++ symfony/northwind/src/Entity/Products.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="¨products")
+ */
+class Products 
+{
+    /**
+     * @ORM\Column(name="ProductId", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    public function getId(): ?int 
+    { 
+        return $this->id;
+    }
+}
+?>
